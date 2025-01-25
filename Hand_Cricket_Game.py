6e3811a8bfe235698cc2c498fa8d17d477dbf6eb
Venkateswaran-A-G <93259660+Venkateswaran-A-G@users.noreply.github.com>
2025-01-25 18:35:52 +0530

    Update Hand_Cricket_Game.py
    Bat First Done Changes in Bowling should be done

--- Hand_Cricket_Game.py
+++ Hand_Cricket_Game.py
@@ -1,2 +1,37 @@
 import random
 
+def bat_first ():
+    print("You are batting first")
+    print("Enter the number between 1 to 6")
+    total = 0
+    while True:
+        user = int(input())
+        computer = random.randint(1,6)
+        print("Computer's number is ",computer)
+        if user == computer:
+            print("You are out")
+            print("Your total score is ",total)
+            break
+        else:
+            total += user
+            print("Your total score is ",total)
+    return total
+
+def bowl_second (target):
+    print("You are bowling now")
+    print("Enter the number between 1 to 6")
+    total = 0
+    while True:
+        user = int(input())
+        computer = random.randint(1,6)
+        print("Computer's number is ",computer)
+        if user == computer:
+            print("Computer is out")
+            print("Computer's total score is ",total)
+            break
+        else:
+            total += computer
+            print("Computer's total score is ",total)
+            if total > target:
+                break
+    return total
